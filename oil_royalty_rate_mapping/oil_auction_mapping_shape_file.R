63d3acb3c9d7a0fa2e74affb470efe211c4080ef
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-24 21:38:01 -0400

    Update oil_auction_mapping_shape_file.R

--- oil_royalty_rate_mapping/oil_auction_mapping_shape_file.R
+++ oil_royalty_rate_mapping/oil_auction_mapping_shape_file.R
@@ -16,16 +16,12 @@ pacman::p_load(
 # USA Level Zipcode for 2015 ----------------------------------------------
 
 tmp2 <- paste0(getwd(),"/oil_royalty_rate_mapping")
-url2 <- "http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_zcta510_500k.zip"
-file <- basename(url2)
-download.file(url2, file)
-unzip(file, exdir = tmp2)
+# url2 <- "http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_zcta510_500k.zip"
+# file <- basename(url2)
+# download.file(url2, file)
+# unzip(file, exdir = tmp2)
 usa <- readOGR(dsn = tmp2, layer = "cb_2015_us_zcta510_500k", encoding = "UTF-8")
 names(usa)[1] = "zipcode"
-
-# url3 <- "http://www.unitedstateszipcodes.org/zip_code_database.csv"
-# file3 <- basename(url3)
-# download.file(url3, file3)
 
 all_usa_zip <- read_csv("oil_royalty_rate_mapping/zip_code_database.csv")
 
@@ -33,7 +29,7 @@ all_usa_zip <- read_csv("oil_royalty_rate_mapping/zip_code_database.csv")
 # Get a specific state ----------------------------------------------------
 
 specific_state <- all_usa_zip %>%
-    filter(state == "TX") %>%
+    filter(state == "LA") %>%
     select(zip, primary_city, county)
 
 colnames(specific_state) <- c("zipcode", "City", "County")
