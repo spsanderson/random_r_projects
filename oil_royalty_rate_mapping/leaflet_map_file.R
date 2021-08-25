22c6749293157dc7a853a5901caeceb9268547bd
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-24 21:51:18 -0400

    Update leaflet_map_file.R

--- oil_royalty_rate_mapping/leaflet_map_file.R
+++ oil_royalty_rate_mapping/leaflet_map_file.R
@@ -36,7 +36,7 @@ final_tbl %>% glimpse()
 # Map leaflet -------------------------------------------------------------
 # You will have to manually set these to the appropriate values for each 
 # state, you can get them coordinates off of maps.google.com
-# the smaller the sv_zoom the furhter out you are looking, so a higher number
+# the smaller the sv_zoom the further out you are looking, so a higher number
 # will zoom you in further to the ground level
 sv_lat <- 31.2957899
 sv_lon <- -92.5523868
@@ -44,4 +44,12 @@ sv_zoom <- 7
 
 leaflet(state_shape) %>%
     setView(lng = sv_lon, lat = sv_lat, zoom = sv_zoom) %>%
-    addTiles(group = "OSM (default)")
+    addTiles(group = "OSM (default)") %>%
+    addPolygons(
+        data = state_shape
+        #, fillColor = ~pal(dsch_bin)
+        , fillOpacity = 0.7
+        # , color = "#BDBDC3"
+        , weight = 0.7
+        #, popup = popup
+    )
