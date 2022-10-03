9095a3185dea59c9a0058fd0fea2d64abdcca03e
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-10-03 13:26:06 -0400

    Update tidy_stat_benchmarking.R

--- benchmarking/tidy_stat_benchmarking.R
+++ benchmarking/tidy_stat_benchmarking.R
@@ -1,24 +1,45 @@
+library(rbenchmark)
+library(data.table)
 library(TidyDensity)
 library(dplyr)
-library(rbenchmark)
 
+# The function
+func <- IQR
+func_chr <- deparse(substitute(IQR))
+
+# Get the interesting vector, well for this anyways
 x <- mtcars$mpg
 
-te <- tidy_bootstrap(x) %>%
+# Bootstrap the vector (2k simulations is default)
+tb <- tidy_bootstrap(x) %>%
   bootstrap_unnest_tbl()
+
+# Make a list object splitting on sim_number
+df_tbl <- tb %>% 
+  split(.$sim_number) %>%
+  map(~ .x %>% pull(y))
+
+# Make a data.table object
+tbd <-  tb %>%
+  as.data.table()
 
 benchmark(
   "tibble" = {
-    t <- tidy_stat_tbl(te, y, IQR, "tibble")
+    t <- tidy_stat_tbl(tb, y, IQR, "tibble")
   },
-  "tibble2" = {
-    t2 <- tidy_stat_tbl2(te,y,IQR,"tibble")
+  "new_func" = {
+    d <- melt(
+      tbd[, as.list(func(y)), sim_number][order(sim_number)],  
+      id.var = "sim_number",  
+      value.name = func_chr
+    ) %>%
+      as_tibble()
   },
   "sapply" = {
-    s <- tidy_stat_tbl(te, y, IQR, "vector")
+    s <- tidy_stat_tbl(tb, y, IQR, "vector")
   },
   "lapply" = {
-    l <- tidy_stat_tbl(te, y, IQR, "list")
+    l <- tidy_stat_tbl(tb, y, IQR, "list")
   },
   replications = 25,
   columns = c("test","replications","elapsed","relative","user.self","sys.self")
