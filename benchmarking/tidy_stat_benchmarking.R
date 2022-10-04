4b5ab608bbe86a7d0752b210c9320ebc2f3c1e79
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-10-03 20:36:16 -0400

    Update tidy_stat_benchmarking.R

--- benchmarking/tidy_stat_benchmarking.R
+++ benchmarking/tidy_stat_benchmarking.R
@@ -1,11 +1,6 @@
 library(rbenchmark)
-library(data.table)
 library(TidyDensity)
 library(dplyr)
-
-# The function
-func <- IQR
-func_chr <- deparse(substitute(IQR))
 
 # Get the interesting vector, well for this anyways
 x <- mtcars$mpg
@@ -14,28 +9,12 @@ x <- mtcars$mpg
 tb <- tidy_bootstrap(x) %>%
   bootstrap_unnest_tbl()
 
-# Make a list object splitting on sim_number
-df_tbl <- tb %>% 
-  split(.$sim_number) %>%
-  map(~ .x %>% pull(y))
-
-# Make a data.table object
-tbd <-  tb %>%
-  as.data.table()
-
 benchmark(
   "tibble" = {
     t <- tidy_stat_tbl(tb, y, IQR, "tibble")
   },
-  "new_func" = {
-    d <- melt(
-      tbd[, as.list(func(y)), sim_number],  
-      id.var = "sim_number",  
-      value.name = func_chr
-    ) %>%
-      as_tibble() %>%
-      rename(name = variable) %>%
-      arrange(sim_number, name)
+  "data.table" = {
+    d <- tidy_stat_tbl(tb, y, IQR, .use_data_table = TRUE, type = 7)
   },
   "sapply" = {
     s <- tidy_stat_tbl(tb, y, IQR, "vector")
