493758e819f12ff02f1a1f7252f5b3b5a09d698e
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2023-03-08 10:09:54 -0500

    Update ny_home_heating_oil.R

--- ny_home_heating_oil/ny_home_heating_oil.R
+++ ny_home_heating_oil/ny_home_heating_oil.R
@@ -4,7 +4,7 @@ url  <- "https://www.eia.gov/opendata/qb.php?sdid=PET.W_EPD2F_PRS_SNY_DPG.W"
 page <- xml2::read_html(url)
 node <- rvest::html_node(
     x = page
-    , xpath = "/html/body/div[1]/div/section/div/div/div[3]/div[1]/table"
+    , xpath = "/html/body/div[1]/section/div/div/div[2]/div[1]/table"
 )
 ny_tbl <- node %>%
     rvest::html_table() %>%
@@ -17,5 +17,4 @@ ny_tbl <- node %>%
 ny_tbl %>%
     timetk::plot_time_series(.date_var = period, .value = value)
 
-ny_tbl %>%
-    timetk::plot_time_series_cv_plan(.date_var = period, .value = value)
+
