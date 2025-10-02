232863dd3c789a2db91e9d629adf433e0118ba7c
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2025-10-02 09:03:51 -0400

    Update hai_data_transform.R

--- healthyr_ai_examples/hai_data_transform.R
+++ healthyr_ai_examples/hai_data_transform.R
@@ -12,7 +12,7 @@ lo <- n * l
 
 date_seq <- seq.Date(from = as.Date("2013-01-01"), length.out = lo, by = "month")
 date_seq
-val_seq <- replicate(n = l, sample(0:10, n)) |> as.vector() |> as.double()
+val_seq <- replicate(n = l, sample(1:10, n)) |> as.vector() |> as.double()
 val_seq
 
 data_tbl <- tibble(
@@ -27,7 +27,7 @@ rec_obj
 box_cox_df <- hai_data_transform(
     .recipe_object = rec_obj,
     value,
-    .type_of_scale = "boxcox",
+    .type_of_scale = "boxcox"
   )$scale_rec_obj %>%
     get_juiced_data() %>%
     mutate(transformation = "boxcox")
@@ -100,10 +100,17 @@ combined_tbl <- bind_rows(
   sqrt_tbl,
   yeojohnson_tbl,
   data_tbl %>% mutate(transformation = "original_value") 
-)
+) |>
+  mutate(
+    transformation = factor(
+      transformation, 
+      levels = summarize(combined_tbl, mean_val = mean(value), .by = transformation) |> arrange(desc(mean_val)) |>
+        pull(transformation)
+    )
+  )
 
 combined_tbl |>
-  ggplot(aes(x = date_col, y = value)) +
+  ggplot(aes(x = date_col, y = value, group = transformation)) +
   geom_point(aes(color = transformation), size = 2, alpha = 0.6) +
   geom_line(aes(color = transformation), linewidth = 1, alpha = 0.6) +
 #  facet_wrap(~ transformation, scales = "free_y") +
@@ -115,14 +122,16 @@ combined_tbl |>
   ) +
   theme_minimal()
 
-p1 <- combined_tbl |>
+combined_tbl |>
   ## facet box plots of the value column by name column
   ggplot(aes(x = transformation, y = value, group = transformation)) +
   geom_boxplot(aes(color = transformation, group = transformation)) +
   geom_jitter(aes(color = transformation, group = transformation), width = 0.3, alpha = .3) +
   labs(
-    x = "",
-    y = "Value"
+    x = "Transformation",
+    y = "Value",
+    title = "Original vs. Transformed Data using HealthyR.ai",
+    subtitle = "Various Data Transformations (Box-Cox, Yeo-Johnson, Log, Sqrt, B-Spline, Natural Spline, ReLU"
   ) +
   theme_classic() +
   theme(legend.position = "none")  +
@@ -130,24 +139,3 @@ p1 <- combined_tbl |>
     # rotate x axis labels
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
-
-p2 <- combined_tbl |>
-  filter(transformation == "original_value") |>
-  select(value, transformation) |>
-  mutate(transformation = "Original Data") |>
-  ggplot(aes(x = transformation, y = value, group = transformation)) +
-  geom_boxplot() +
-  geom_jitter(aes(x = transformation, y = value), width = 0.3, alpha = .3, color = "blue") +
-  theme_classic() +
-  labs(
-    x = "",
-    y = ""
-  ) +
-  theme(
-    # rotate x axis labels
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-(p1 + p2) +
-    plot_annotation(title = "Transformed vs Original Data using HealthyR.ai",
-                    subtitle = "Function: hai_data_transform()")
